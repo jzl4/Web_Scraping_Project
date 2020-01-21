278d791b13af4fd7fc74ac326232caac82028e6e
Joe Lu <Joe.Zhou.Lu@gmail.com>
2020-01-20 19:36:49 -0500

    Added bash script for creating and updating this repo

---
+++ Github_Commands_for_Web_Scraping_Project.bash
@@ -0,0 +1,11 @@
+# Creating and uploading my first iteration of this project
+cd C:/Main/NYCDSA/Web_Scraping_Project
+git init
+git status
+git add --all
+git commit --all -m "Scrapy shell commands for grabbing Reuters news titles"
+git log --oneline
+# I created a repo on my Github named Web_Scraping_Project
+git remote add origin https://github.com/jzl4/Web_Scraping_Project
+git push origin master
+
