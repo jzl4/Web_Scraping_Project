de714cdb19baadd8f44cf416ff6ce52f85ac88cd
Joe Lu <Joe.Zhou.Lu@gmail.com>
2020-01-20 20:54:40 -0500

    Updated pipelines and settings files. Able to produce csv file of 2x20 (titles + descriptions) x (20 articles) now!

--- Reuters/Reuters/settings.py
+++ Reuters/Reuters/settings.py
@@ -64,9 +64,9 @@ DOWNLOAD_DELAY = 3
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'Reuters.pipelines.ReutersPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'Reuters.pipelines.ReutersPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
